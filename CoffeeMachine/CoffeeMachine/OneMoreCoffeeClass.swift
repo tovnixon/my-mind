5d97a9ac2aad1cacc6adc09e6be7bf31673a787d
Anna Oksanichenko <oksanichenkoann@gmail.com>
2020-09-24 08:52:29 +0300

    fix funcs, have issues with func makeADrink

--- CoffeeMachine/CoffeeMachine/OneMoreCoffeeClass.swift
+++ CoffeeMachine/CoffeeMachine/OneMoreCoffeeClass.swift
@@ -15,7 +15,7 @@ import Foundation
 
 enum MyCoffeeComponentType{
     case water, milk, beans
-    var min: Int {return 20}
+   
 }
 // у компонента есть тип и обьем
 class ComponentContain {
@@ -26,27 +26,22 @@ class ComponentContain {
         self.type = type
         self.volume = volume
     }
-}
-// все компоненты, которые используются в приготовлении напитка
-class AllComponents{
     
-    var water: Int
-    var beans: Int
-    var milk: Int
-    init(water: Int, beans: Int, milk: Int){
-        self.water = water
-        self.beans = beans
-        self.milk = milk
+    func addVolume(extraVol: Int){
+        volume += extraVol
+    }
+    func removeVolume(extraVol: Int){
+        volume -= extraVol
     }
 }
+// все компоненты, которые используются в приготовлении напитка
 //у напитка есть имя и компоненты
 class myDrink{
     let name: String
-    var allComponents: AllComponents
-    var components2 = [ComponentContain]()
-    init(name: String, allComponents: AllComponents) {
+    var components = [ComponentContain]()
+    init(name: String, components : [ComponentContain]) {
         self.name = name
-        self.allComponents = allComponents
+       self.components = components
     }
 }
 
@@ -55,65 +50,69 @@ protocol CMachineProtocol {
     func addSomeComponent(some: MyCoffeeComponentType)-> String
  //   func addNewComponent<T>(some: T){}
    func canMakeADrink(drink: myDrink)->String
-    func hasEnoughComponentOfType(type: MyCoffeeComponentType)-> Bool
+    func hasEnoughComponentOfType(type: ComponentContain)-> Bool
     func refreshTrash()-> String
 }
 
 class CMachine: CMachineProtocol {
    
   // изначально кофе машина не заполнена
-    var beansRawValue = 0
-    var milkRawValue = 0
-    var waterRawValue = 0
+   
     let valueForAdd = 100
-    var components2 = [ComponentContain]()
-  //  var allComponents: AllComponents
+    var availableComponents = [ComponentContain]()
+
     
     var trash = 0
     var trashCapacity = 50
-//    init(allComponents: AllComponents) {
-//        self.allComponents = allComponents
-//    }
-//
+
+    private func getComponentByType(_ type: MyCoffeeComponentType) -> ComponentContain? {
+       for component in availableComponents {
+        if component.type == type {
+           return component
+          }
+        }
+      return nil
+    }
     
     
     func addSomeComponent(some: MyCoffeeComponentType)-> String{
-        switch some {
-        case .beans: beansRawValue += valueForAdd
-        case .milk: milkRawValue += valueForAdd
-        case .water: waterRawValue += valueForAdd
-        }
+        let component : ComponentContain = getComponentByType(some)!
+        component.addVolume(extraVol: valueForAdd)
+        
         return "Component \(some) added"
     }
     
-    func hasEnoughComponentOfType(type: MyCoffeeComponentType)-> Bool{
-//        for comp in components2{
-//            if comp < comp.minvol {}
-//        }
-        if milkRawValue < type.min { return false }
-        return true
+    func hasEnoughComponentOfType(type: ComponentContain)-> Bool{
+        
+        for components in availableComponents {
+            if components.volume < type.minvol {return false}
+            return true
+        }
     }
+    
     
     func canMakeADrink(drink: myDrink)->String{
-        for compon in components2{
-            for c in drink.components2{
-                if compon < c {
-                    
+        for component in availableComponents {
+            for c in drink.components {
+                if component.volume < c.volume {
+                    return "Not enough \(component)"
                 }
+                if trash > trashCapacity {return "Refresh trash"}
             }
         }
-        if beansRawValue < drink.allComponents.beans { return "Not enough beans" }
-        if waterRawValue < drink.allComponents.water {return "Not enough water"}
-        if milkRawValue < drink.allComponents.milk {return "Not enough milk"}
-        if trash > trashCapacity {return "Refresh trash"}
-    }
+        return "Let`s make a drink!"
+        }
+        
+    
     
     func letsMakeDrink(drink: myDrink)-> String{
+        for component in availableComponents{
+            for c in drink.components{
+                component.removeVolume(extraVol: c )
+            }
+        }
+        trash += drink.components[ComponentContain.init(type: .beans, vol)]
         
-        beansRawValue -= drink.allComponents.beans
-        waterRawValue -= drink.allComponents.water
-        milkRawValue -= drink.allComponents.milk
-        trash += drink.allComponents.beans
         return "Here is your \(drink.name), bro"
     }
     
@@ -126,7 +125,9 @@ class CMachine: CMachineProtocol {
 
 
 let NCMach = CMachine()
-let res = NCMach.hasEnoughComponentOfType(type: MyCoffeeComponentType)
+
 // let NCMach = CMachine(allComponents: AllComponents(water: 100, beans: 100, milk: 100))
-let amer = myDrink(name: "americano", allComponents: AllComponents(water: 20, beans: 20, milk: 0))
-let capuch = myDrink(name: "capuchino", allComponents: AllComponents(water: 0, beans: 20, milk: 20))
+
+let espr =  myDrink(name: "esp", components: [ComponentContain.init(type: .milk, volume: 20),ComponentContain.init(type: .beans, volume: 30)])
+
+
