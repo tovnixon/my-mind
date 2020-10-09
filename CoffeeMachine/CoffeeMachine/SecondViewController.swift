2011094f78b2f9dc027cc3483c4f52a3d35307b8
Anna Oksanichenko <oksanichenkoann@gmail.com>
2020-10-09 13:18:28 +0300

    added all constraints

---
+++ CoffeeMachine/CoffeeMachine/SecondViewController.swift
@@ -0,0 +1,48 @@
+//
+//  SecondViewController.swift
+//  CoffeeMachine
+//
+//  Created by Anna Oksanichenko on 08.10.2020.
+//  Copyright © 2020 Anna Oksanichenko. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var waterOutlet: UISlider!
+    @IBOutlet weak var milkOutlet: UISlider!
+    @IBOutlet weak var beansOutlet: UISlider!
+    @IBOutlet weak var trashOutlet: UISlider!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func addWaterButton(_ sender: UIButton) {
+    }
+    @IBAction func addMilkButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func addBeansButton(_ sender: UIButton) {
+    }
+    @IBAction func cleanTrashButton(_ sender: UIButton) {
+    }
+    @IBAction func americanoButton(_ sender: UIButton) {
+    }
+    @IBAction func capuchinoButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func latteButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func flatWhiteButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func warmMilkButton(_ sender: UIButton) {
+    }
+    
+}
